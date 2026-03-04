5a93e021b6d57385e1a40d5c05c2be9e2532c99e
Valeriia Balatska <253850@edu.p.lodz.pl>
2026-03-04 16:33:48 +0000

    initial folder struct

---
+++ isep/DEMOORM/src/main/java/isep/eapli/demoorm/DemoORM.java
@@ -0,0 +1,9 @@
+package isep.eapli.demoorm;
+
+import isep.eapli.demoorm.presentation.MainMenu;
+
+public class DemoORM {
+    public static void main(String[] args) {
+        MainMenu.mainLoop();
+    }
+}
